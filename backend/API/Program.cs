64c041ad9b5ad6bea2b90913ce9f50668bf7fb11
Merzuk <sisicmerzuk@gmail.com>
2024-12-07 19:23:00 +0100

    Enable and Configure CORS Middleware in Program.cs

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof (IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +29,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 try

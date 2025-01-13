2fd1e1922bb62484214216e88c9119ceea9104c2
Benjamin Skula <benjaminskula0@gmail.com>
2025-01-13 07:11:02 +0100

    Created Register form and validation, added reusable text input component, Closes #182, #183, #180

--- frontend/shop/src/app/core/services/init.service.ts
+++ frontend/shop/src/app/core/services/init.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {CartService} from './cart.service';
-import {forkJoin, of} from 'rxjs';
+import {catchError, forkJoin, of} from 'rxjs';
 import { AccountService } from './account.service';
 
 @Injectable({
@@ -10,14 +10,23 @@ export class InitService {
   private cartService = inject(CartService);
   private accountService=inject(AccountService);
 
-  init(){
+  init() {
     const cardId = localStorage.getItem('cart_id');
     const cart$ = cardId ? this.cartService.getCart(cardId) : of(null);
-
+    const user$ = this.accountService.getUserInfo().pipe(
+      catchError(err => {
+        console.error('Error fetching user info:', err);
+        return of(null); // Vrati null ako dođe do greške
+      })
+    );
+  
     return forkJoin({
       cart: cart$,
-      user: this.accountService.getUserInfo()
+      user: user$
     });
   }
+  
+
+
 
 }

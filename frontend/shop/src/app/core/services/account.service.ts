916b56ee246ad50b9bd3dd28c009db5e2fad5071
Benjamin Skula <benjaminskula0@gmail.com>
2025-01-13 04:39:19 +0100

    Added persist user using local storage, added auth interceptor for request handling, Closes #177, #178, #174

--- frontend/shop/src/app/core/services/account.service.ts
+++ frontend/shop/src/app/core/services/account.service.ts
@@ -3,6 +3,7 @@ import { environment } from '../../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Address, User } from '../../shared/models/user';
 import { __values } from 'tslib';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,7 @@ login(values: any)
 {
   let params=new HttpParams();
   params=params.append('useCookies', true);
-  return this.http.post<User>(this.baseUrl+ 'login',values, {params, withCredentials: true});
+  return this.http.post<User>(this.baseUrl+ 'login',values, {params});
 }
 
 
@@ -28,14 +29,18 @@ return this.http.post(this.baseUrl+'account/register',values);
 }
 
 getUserInfo(){
-  return this.http.get<User>(this.baseUrl+'account/user-info',{withCredentials: true}).subscribe({
-    next: user=>this.currentUser.set(user)
-  })
+  return this.http.get<User>(this.baseUrl+'account/user-info').pipe(
+    map(user=>{
+      this.currentUser.set(user);
+      return user;
+    })
+  )
+  
 }
 
 
 logout(){
-  return this.http.post(this.baseUrl+'account/logout',{},{withCredentials: true});
+  return this.http.post(this.baseUrl+'account/logout',{});
 }
 
 updateAddress(address: Address){

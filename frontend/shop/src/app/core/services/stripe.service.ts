9415fc1f3bc23fcf1d3ed3b4fe9e20ce93e68a3d
Benjamin Skula <benjaminskula0@gmail.com>
2025-01-17 16:41:37 +0100

    Added confirm Payment function, Closes #204

--- frontend/shop/src/app/core/services/stripe.service.ts
+++ frontend/shop/src/app/core/services/stripe.service.ts
@@ -1,5 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {
+  ConfirmationToken,
   loadStripe,
   Stripe,
   StripeAddressElement,
@@ -116,6 +117,31 @@ async createPaymentElement(){
   }
 
 
+
+  async confirmPayment(confirmationToken:ConfirmationToken){
+    const stripe=await this.getStripeInstance();
+    const elements=await this.initializeElements();
+    const result=await elements.submit();
+
+
+    if(result.error) throw new Error(result.error.message); 
+
+    const clientSecret= this.cartService.cart()?.clientSecret;
+
+    if(stripe && clientSecret){
+      return await stripe.confirmPayment({
+        clientSecret: clientSecret,
+        confirmParams:{
+          confirmation_token: confirmationToken.id
+        },
+        redirect: 'if_required'
+      })
+
+    }
+    else{
+      throw new Error ('Unable to loda stripe');
+    }
+  }
 
 
   createOrUpdatePaymentIntent() {

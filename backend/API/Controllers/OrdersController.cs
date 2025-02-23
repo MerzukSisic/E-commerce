b6decefcd753f85f9c24d63533a04a09f91128d7
Merzuk <sisicmerzuk@gmail.com>
2025-02-23 21:57:14 +0100

    Added order service, order components, and implemented order service and fixes

--- backend/API/Controllers/OrdersController.cs
+++ backend/API/Controllers/OrdersController.cs
@@ -7,94 +7,89 @@ using Core.Interfaces;
 using Core.Specifications;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 namespace API.Controllers;
+
 [Authorize]
-public class OrdersController(ICartService cartService, IUnitOfWork unit): BaseApiController
+public class OrdersController(ICartService cartService, IUnitOfWork unit) : BaseApiController
 {
-
-[HttpPost]
-public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
-{
-    var email=User.GetEmail();
-
-    var cart= await cartService.GetCartAsync(orderDto.CartId);
-    if(cart==null)return BadRequest("Cart not found");
-
-    if(cart.PaymentIntentId==null) return BadRequest("No payment intent for this order");
-
-    var items=new List<OrderItem>();
-
-    foreach(var item in cart.Items)
+    [HttpPost]
+    public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
     {
-        var productItem=await unit.Repository<Product>().GetByIdAsync(item.ProductId);
+        var email = User.GetEmail();
+ 
+        var cart = await cartService.GetCartAsync(orderDto.CartId);
+        if (cart == null) return BadRequest("Cart not found");
 
-        if(productItem==null) return BadRequest("Problem with the order");
+        if (cart.PaymentIntentId == null) return BadRequest("No payment intent for this order");
 
-        var itemOrdered=new ProductItemOrdered
+        var items = new List<OrderItem>();
+
+        foreach (var item in cart.Items)
         {
-            ProductId=item.ProductId,
-            ProductName=item.Name,
-            PictureUrl=item.PictureUrl
+            var productItem = await unit.Repository<Product>().GetByIdAsync(item.ProductId);
+
+            if (productItem == null) return BadRequest("Problem with the order");
+
+            var itemOrdered = new ProductItemOrdered
+            {
+                ProductId = item.ProductId,
+                ProductName = item.Name,
+                PictureUrl = item.PictureUrl
+            };
+
+            var orderItem = new OrderItem
+            {
+                ItemOrdered = itemOrdered,
+                Price = productItem.Price,
+                Quantity = productItem.QuantityInStock
+            };
+            items.Add(orderItem);
+        }
+
+        var deliveryMethod = await unit.Repository<DeliveryMethod>().GetByIdAsync(orderDto.DeliveryMethodId);
+
+        if (deliveryMethod == null) return BadRequest("No delivery method selected");
+
+        var order = new Order
+        {
+            OrderItems = items,
+            DeliveryMethod = deliveryMethod,
+            ShippingAddress = orderDto.ShippingAddress,
+            Subtotal = items.Sum(x => x.Price * x.Quantity),
+            PaymentSummary = orderDto.PaymentSummary,
+            PaymentIntentId = cart.PaymentIntentId,
+            BuyerEmail = email
         };
 
-        var orderItem=new OrderItem
+        unit.Repository<Order>().Add(order);
+
+        if (await unit.Complete())
         {
-            ItemOrdered=itemOrdered,
-            Price=productItem.Price,
-            Quantity=productItem.QuantityInStock
-        };
-        items.Add(orderItem);
+            return order;
+        }
+
+        return BadRequest("Problem creating order");
     }
 
-    var deliveryMethod=await unit.Repository<DeliveryMethod>().GetByIdAsync(orderDto.DeliveryMethodId);
 
-    if(deliveryMethod==null) return BadRequest("No delivery method selected");
-
-    var order=new Order
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrdersForUser()
     {
-        OrderItems=items,
-        DeliveryMethod=deliveryMethod,
-        ShippingAddress=orderDto.ShippingAddress,
-        Subtotal=items.Sum(x=>x.Price*x.Quantity),
-        PaymentSummary=orderDto.PaymentSummary,
-        PaymentIntentId=cart.PaymentIntentId,
-        BuyerEmail=email
+        var spec = new OrderSpecification(User.GetEmail());
 
-    };
+        var orders = await unit.Repository<Order>().ListAysnc(spec);
 
-    unit.Repository<Order>().Add(order);
-
-    if(await unit.Complete())
-    {
-        return order;
+        var ordersToReturn = orders.Select(o => o.ToDto()).ToList();
+        return Ok(ordersToReturn);
     }
-    return BadRequest("Problem creating order");
 
-
-
-}
-
-
-[HttpGet]
-public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrdersForUser()
-{
-    var spec=new OrderSpecification(User.GetEmail());
-
-    var orders=await unit.Repository<Order>().ListAysnc(spec);
-
-    var ordersToReturn= orders.Select(o=>o.ToDto()).ToList();
-    return Ok(ordersToReturn);
-}
-[HttpGet("{id:int}")]
-
-public async Task<ActionResult<OrderDto>> GetOrderById(int id)
-{
-    var spec=new OrderSpecification(User.GetEmail(), id);
-    var order=await unit.Repository<Order>().GetEntityWithSpec(spec);
-    if(order==null) return NotFound();
-    return order.ToDto();
-}
-
-
-
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<OrderDto>> GetOrderById(int id)
+    {
+        var spec = new OrderSpecification(User.GetEmail(), id);
+        var order = await unit.Repository<Order>().GetEntityWithSpec(spec);
+        if (order == null) return NotFound();
+        return order.ToDto();
+    }
 }

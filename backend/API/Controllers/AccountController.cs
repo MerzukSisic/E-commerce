b5b95b6a1fc64467d372b0e5a3586ad3d7415115
Merzuk <sisicmerzuk@gmail.com>
2025-03-17 05:21:14 +0100

    Modified Account Controller for Email Verification, Closes #323

--- backend/API/Controllers/AccountController.cs
+++ backend/API/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
+using System.Net;
 using System.Security.Claims;
 using API.DTOs;
 using API.Extensions;
 using Core.Entities;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +11,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(SignInManager<AppUser> signInManager) : BaseApiController
+public class AccountController(SignInManager<AppUser> signInManager, IEmailService emailService) : BaseApiController
 {
     [HttpPost("register")]
     public async Task<ActionResult> Register(RegisterDto registerDto)
@@ -59,7 +61,8 @@ public class AccountController(SignInManager<AppUser> signInManager) : BaseApiCo
             user.LastName,
             user.Email,
             Address = user.Address?.toDto(),
-            Roles=User.FindFirstValue(ClaimTypes.Role)
+            Roles = User.FindFirstValue(ClaimTypes.Role),
+            user.EmailConfirmed
         });
     }
 
@@ -94,5 +97,45 @@ public class AccountController(SignInManager<AppUser> signInManager) : BaseApiCo
         if (!result.Succeeded) return BadRequest("Problem updating Address");
 
         return Ok(user.Address.toDto());
+    }
+
+    [Authorize]
+    [HttpPost("resend-confirmation")]
+    public async Task<ActionResult> ResendConfirmationEmail()
+    {
+        var userEmail = User.FindFirstValue(ClaimTypes.Email);
+        if (string.IsNullOrEmpty(userEmail))
+            return Unauthorized("User is not authenticated");
+
+        var user = await signInManager.UserManager.FindByEmailAsync(userEmail);
+        if (user == null)
+            return BadRequest("User is not found");
+        if (user.EmailConfirmed)
+            return BadRequest("Email is already confirmed");
+
+        var token = await signInManager.UserManager.GenerateEmailConfirmationTokenAsync(user);
+        var frontendUrl = "https://localhost:4200";
+
+        var encodedToken = WebUtility.UrlEncode(token);
+        var confirmationLink =
+            $"{frontendUrl}/confirm-email?email={WebUtility.UrlEncode(user.Email)}&token={encodedToken}";
+
+        await emailService.SendEmailAsync(user.Email, "Verify Your Email", confirmationLink);
+
+        return Ok(new { message = "Verification email sent." });
+    }
+
+    [HttpPost("confirm-email")]
+    public async Task<ActionResult> ConfirmEmail([FromBody] EmailDto dto)
+    {
+        var user = await signInManager.UserManager.FindByEmailAsync(dto.Email);
+        if (user == null)
+            return BadRequest("User not found.");
+
+        var result = await signInManager.UserManager.ConfirmEmailAsync(user, dto.Token);
+        if (!result.Succeeded)
+            return BadRequest(new { errors = result.Errors.Select(e => e.Description) });
+
+        return Ok(new { message = "Email confirmed successfully." });
     }
 }

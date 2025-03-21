d8ef01395812d2bdda40ab9fde682508a8123d71
Merzuk <sisicmerzuk@gmail.com>
2025-03-21 05:47:33 +0100

    Fixed

--- frontend/shop/src/environments/environment.development.ts
+++ frontend/shop/src/environments/environment.development.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
   hubUrl: 'https://localhost:5001/hub/notifications',
-
-  stripePublicKey: 'pk_test_51Qh7hNAscngZmeE7SgHike7TiSwezOggbAjRAAd44wNnC0tDO85IyPyFykBmVpXd6Hk0PYms7SoWU0warUqt8lsO001h6L8qDC'
+  stripePublicKey: ''
 };
--- frontend/shop/src/environments/environment.ts
+++ frontend/shop/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'api/',
   hubUrl:'hub/notifications',
-  stripePublicKey: 'pk_test_51Qh7hNAscngZmeE7SgHike7TiSwezOggbAjRAAd44wNnC0tDO85IyPyFykBmVpXd6Hk0PYms7SoWU0warUqt8lsO001h6L8qDC'
+  stripePublicKey: ''
 };
